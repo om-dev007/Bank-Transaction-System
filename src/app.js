6e272d7cfbea95373cd4d82f56e617fc17c83e68
om-dev007 <officialomnarayan@gmail.com>
2026-03-04 23:38:41 +0530

    add cookie parser for token

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import e from "express";
+import router from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = e();
+
+app.use(e.json())
+
+app.use(cookieParser())
+
+app.use("/api/auth", router)
 
 export default app;
