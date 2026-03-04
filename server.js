c6776796ba4e41594c069dc970dac54502a228a5
om-dev007 <officialomnarayan@gmail.com>
2026-03-04 23:29:06 +0530

    dotenv config

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+import "dotenv/config";
 import app from "./src/app.js";
 import { connectDb } from "./src/config/db.js";
-import dotenv from 'dotenv'
-dotenv.config();
 
-connectDb()
+connectDb();
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
+  console.log(`Server is running on ${port}`);
+});
