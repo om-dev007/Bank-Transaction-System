ea40a99b832c2c507e87b4b668fe8115d219ac39
om-dev007 <officialomnarayan@gmail.com>
2026-03-08 12:28:47 +0530

    account routes add

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import e from "express";
 import router from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import accountRoutes from "./routes/account.routes.js";
 
 const app = e();
 
@@ -9,5 +10,6 @@ app.use(e.json())
 app.use(cookieParser())
 
 app.use("/api/auth", router)
+app.use("/api/account", accountRoutes)
 
 export default app;
